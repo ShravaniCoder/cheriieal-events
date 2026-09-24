66764004bea7adb8188ca143db61c53e012e5bba
ShravaniCoder <shravanichendwankar@gmail.com>
2026-09-24 16:23:57 +0530

    images of services

--- src/data.js
+++ src/data.js
@@ -16,12 +16,12 @@ export const images = {
   stage: "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&fit=crop&w=1200&q=86",
   dinner: "https://images.unsplash.com/photo-1515003197210-e0cd71810b5f?auto=format&fit=crop&w=1200&q=86",
   meeting: "https://images.unsplash.com/photo-1556761175-b413da4baf72?auto=format&fit=crop&w=1200&q=86",
-  hospitality: "/images/Food.png",
+  hospitality: "/images/Foood.png",
   floral: "/images/cor.png",
   contact: "/images/ContactHH.png",
   heroF: "/images/HomeF.png",
   service: "/images/ServiceB.png",
-  serviceFo: "/images/ServiceFo.png"
+  serviceFo: "/images/Stage.png"
 };
 
 export const personalEvents = [
